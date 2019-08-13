02d61daad8bd2833a0d38068e1b369fc737e4ca8
Charles Boadu <ekowcharles@gmail.com>
2019-08-12 23:59:38 -0500

    Retrieve array from step object instead

--- stp.go
+++ stp.go
@@ -3,6 +3,6 @@ package morph
 import "github.com/go-pg/pg/v9"
 
 type Stepable interface {
-	Up(db *pg.DB, st []interface{}) error
-	Down(db *pg.DB, st []interface{}) error
+	Up(db *pg.DB) error
+	Down(db *pg.DB) error
 }
